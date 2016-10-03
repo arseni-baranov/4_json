4eecaf2d5b664879548d61af6edf3626a31e7041
coding@justlike.pro <coding@justlike.pro>
2016-10-03 16:04:39 +0500

    сделано

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,24 @@
-import json
-
+import json, os.path
 
 def load_data(filepath):
-    pass
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    data_file = current_dir + '\\' + filepath
+    file = open(data_file, "r", encoding="utf-8")
+
+    return json.loads(file.read())
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+
+        filename = ''
+
+        while not os.path.isfile(filename):
+            try:
+                filename = input('Введите название json файла: ')
+                pretty_print_json(load_data(filename))
+            except FileNotFoundError:
+                print('Данного файла не существует в директории с программой')
